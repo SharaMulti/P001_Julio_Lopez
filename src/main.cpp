984e783761cb9f9ad20445a35eb6010b84dd54ef
SharaMulti <jjuulliiooooooooo@gmail.com>
2025-02-21 08:21:46 -0600

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+Amerike university
+Author: Julio López
+Project n:1
+Date: 21/02/25
+Description: Present "Hello World" in screen*/
+
+#include <iostream>
+ 
+using namespace std;
+
+
+int main () {
+
+    cout<< "Hello World!";
+
+    return 0;
+}
